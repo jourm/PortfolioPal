e2b0b8b7d0926cf9bfb030af02d1edc90148c2a4
jourm <jourm91@gmail.com>
2020-11-24 19:58:47 +0100

    Add route for adding new symbols to your portfolio, with matching template

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 import yfinance as yf
 import os
+from flask_pymongo import PyMongo, pymongo
+from secret import MONGO_URI
 from os import path
+
 
 stocks = [
     ["MSFT", 10, 175],
@@ -13,6 +16,9 @@ stocks = [
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MONGO_DBNAME'] = 'portfoliopal'
+app.config['MONGO_URI'] = MONGO_URI
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
@@ -23,6 +29,15 @@ def home():
 
     return render_template('dashboard.html', stocks=stocks)
 
+@app.route('/add_symbol', methods=['GET', 'POST'])
+def add_symbol():
+    if request.method == 'GET':
+        return render_template('add_symbol.html')
+
+    elif request.method == 'POST':
+        symbol = request.form['symbol']
+        amount = request.form['amount']
+        purchase_date = request.form['purchase_date']
 
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP", "127.0.0.1"),
